30bf27c41e6169f9d1d26bc76e5b1bb5538232cc
Ashish <chandoshiashu@gmail.com>
2026-09-08 21:49:09 +0530

    FIX 2: GOOGLE AUTH REDIRECT

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const ITINERARY = mongoose.model('ITINERARY', ActivitySchema);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'https://roamio-trip.vercel.app/auth/google/callback',
     passReqToCallback: true
 },
 async (req, accessToken, refreshToken, profile, done) => {
