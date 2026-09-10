cb5dcb92c6816a9239897c65af9b75dbc714427a
Ashish <chandoshiashu@gmail.com>
2026-09-10 22:50:59 +0530

    FIX: PROFILE PIC UPLOADER IMPLEMENTED

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('trust proxy', 1);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json( {limit: '50mb'} ));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(cookieParser());
