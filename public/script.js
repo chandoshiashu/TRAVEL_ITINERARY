0e1ff4b2bc47ea8ed69ce2d25fa5f0a873b453b0
Ashish <chandoshiashu@gmail.com>
2026-09-12 22:37:21 +0530

    FIX 21: STYLING

--- public/script.js
+++ public/script.js
@@ -883,7 +883,7 @@ function openActivity(name, image_url) {
    	window.location.href = `/activity/${encodeURIComponent(name)}?image_url=${encodeURIComponent(image_url)}`;
    	setTimeout(() => {
         hideLoader();
-    }, 5000);
+    }, 2500);
 }
 
 
@@ -894,7 +894,7 @@ function openBlog(name) {
 
 	setTimeout(() => {
         hideLoader();
-    }, 5000);
+    }, 2500);
 }
 
 
